1630e1739346f997f87ca14b850db0029936dd70
Ahmed Lamlih <contact.ahmed.lamlih@gmail.com>
2026-02-11 06:39:11 +0100

    debug 2

--- docker/start.sh
+++ docker/start.sh
@@ -2,27 +2,18 @@
 set -e
 
 echo "Fixing permissions on var/..."
-mkdir -p /var/www/html/var/cache /var/www/html/var/log
+mkdir -p /var/www/html/var/cache/prod /var/www/html/var/log
 chown -R www-data:www-data /var/www/html/var
-chmod -R ug+rwX /var/www/html/var
+chmod -R 775 /var/www/html/var
 
 echo "DB URL is: ${DATABASE_URL:-NOT_SET}"
 
-echo "Doctrine validate:"
-php /var/www/html/bin/console doctrine:schema:validate --env=prod
+echo "Clearing + warming cache as www-data..."
+su -s /bin/sh www-data -c "php /var/www/html/bin/console cache:clear --env=prod"
+su -s /bin/sh www-data -c "php /var/www/html/bin/console cache:warmup --env=prod"
 
-echo "Doctrine dump sql:"
-php /var/www/html/bin/console doctrine:schema:update --dump-sql --env=prod
-
-echo "Applying schema update:"
-php /var/www/html/bin/console doctrine:schema:update --force --env=prod
-
-exec apache2-foreground
-
-echo "Creating/updating DB schema as www-data (temporary)..."
-if ! su -s /bin/sh www-data -c "php /var/www/html/bin/console doctrine:schema:update --force --env=prod"; then
-  echo "SCHEMA UPDATE FAILED (but continuing to start Apache)"
-fi
+echo "Updating DB schema as www-data..."
+su -s /bin/sh www-data -c "php /var/www/html/bin/console doctrine:schema:update --force --env=prod"
 
 echo "Starting Apache..."
 exec apache2-foreground
